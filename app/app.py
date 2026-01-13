d2f54521ee7c2b604d73af4f6152fa6c6052ccdf
amhg <androzka@yahoo.com>
2026-01-13 12:07:39 -0600

    Add auth_type to login response

--- app/app.py
+++ app/app.py
@@ -10,7 +10,9 @@ def health():
 
 @app.route("/login", methods=["GET"])
 def login():
-    return {"message": "login page"}
+    return {"message": "login page",
+            "auth_type": "basic"
+            }
 
 if __name__ == "__main__":
     app.run(debug=True)

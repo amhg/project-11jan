de00592f04f2e0e19c1860bd55b0f2f55b23567a
amhg <androzka@yahoo.com>
2026-01-19 10:47:41 -0600

    Initial Flask app with health endpoint

--- app/app.py
+++ app/app.py
@@ -1,16 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return {"status": "ok",
-            "service": "auth-api"
-            }
-
-@app.route("/login", methods=["GET"])
-def login():
-    return {"message": "login page"}
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     app.run(debug=True)

fc134e4829d9387c409a665e43415bf7aab71067
amhg <androzka@yahoo.com>
2026-01-20 14:52:06 -0600

    Revert "Initial Flask app with health endpoint"
    This reverts commit de00592f04f2e0e19c1860bd55b0f2f55b23567a.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return {"status": "ok"}
+    return {"status": "ok",
+            "service": "auth-api"
+            }
+
+@app.route("/login", methods=["GET"])
+def login():
+    return {"message": "login page"}
 
 if __name__ == "__main__":
     app.run(debug=True)

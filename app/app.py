e7448f2eecad5dc655f791152ca0b908d5b85e8b
amhg <androzka@yahoo.com>
2026-01-21 09:27:14 -0600

    Break login endpoint

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ def health():
 
 @app.route("/login", methods=["GET"])
 def login():
-    return {"message": "login page"}
+    return {"message": "BROKEN LOGIN"}
 
 if __name__ == "__main__":
     app.run(debug=True)

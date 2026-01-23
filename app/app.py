239a28860ad1308d9f11fab60041a05c073f441a
amhg <androzka@yahoo.com>
2026-01-23 09:05:27 -0600

    Release 1.0.0

--- app/app.py
+++ app/app.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+VERSION = "1.0.0"
+
+@app.route("/")
+def info():
+    return {
+        "service": "health-service",
+        "version": VERSION
+    }
+
 @app.route("/health")
 def health():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)

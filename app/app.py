c1237a3b3e5c142e3a587171e84ee54a5b91dee9
amhg <androzka@yahoo.com>
2026-01-30 15:17:17 -0600

    added logout endpoint

--- app/app.py
+++ app/app.py
@@ -10,5 +10,9 @@ def health():
 def login():
     return {"message": "login page"}
 
+@app.route("/logout", methods=["GET"])
+def login():
+    return {"message": "logout page"}
+
 if __name__ == "__main__":
     app.run(debug=True)
